baab459a4d66541e93f1e4cc798cfdcb7b48373a
Evgenii Khramkov <hey@ewgenius.me>
2023-01-26 21:36:42 +0400

    update repo links

--- apps/example/components/ExampleLayout.tsx
+++ apps/example/components/ExampleLayout.tsx
@@ -37,7 +37,7 @@ export const ExampleLayout: FC<
 
           <a
             href="https://www.npmjs.com/package/tailwindcss-variable-themes"
-            className="underline opacity-50 hover:no-underline hover:opacity-100"
+            className="underline opacity-50 hover:no-underline hover:opacity-100 transition-opacity duration-300 flex gap-1 underline-offset-2 items-center"
           >
             <svg
               className="w-4 h-4"
@@ -56,11 +56,12 @@ export const ExampleLayout: FC<
               <polyline points="3.29 7 12 12 20.71 7"></polyline>
               <line x1="12" y1="22" x2="12" y2="12"></line>
             </svg>
+            <span>NPM</span>
           </a>
 
           <a
             href="https://github.com/ewgenius/tailwindcss-variable-themes"
-            className="underline opacity-50 hover:no-underline hover:opacity-100"
+            className="underline opacity-50 hover:no-underline hover:opacity-100 transition-opacity duration-300 flex gap-1 underline-offset-2 items-center"
           >
             <svg
               className="w-4 h-4"
@@ -77,6 +78,7 @@ export const ExampleLayout: FC<
               <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path>
               <path d="M9 18c-4.51 2-5-2-7-2"></path>
             </svg>
+            <span>GitHub</span>
           </a>
         </nav>
         <h1 className="text-3xl font-bold">
